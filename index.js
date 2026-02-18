edbfe34e0eaca440a7da663ef23e0d5e1b79da8e
Aaryaarya <aryashree2004@gmail.com>
2026-02-18 22:45:29 +0530

    connect railway db

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+require("dotenv").config();
 const express = require("express");
+const mysql = require("mysql2/promise");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+let pool;
+
+async function connectDB() {
+  try {
+    pool = mysql.createPool(process.env.DATABASE_URL);
+    console.log("Connected to Railway MySQL ✅");
+  } catch (err) {
+    console.error("DB Connection Failed ❌", err);
+  }
+}
+
 app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
+  res.send("Backend + Railway Connected 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/test-db", async (req, res) => {
+  const [rows] = await pool.query("SELECT 1 + 1 AS result");
+  res.json(rows);
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+  });
 });
